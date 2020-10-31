1b8dd8cc75d3af701e044471346b4e8d4dd77aea
Miyo Alpízar <miyo.naval@gmail.com>
2020-10-31 09:54:10 -0600

    Initial Commit

---
+++ LinkedinSwipe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LinkedinSwipe
+//
+//  Created by Miyo Alpízar on 31/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LinkedinSwipe/LinkedinSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedinSwipeApp.swift
+//  LinkedinSwipe
+//
+//  Created by Miyo Alpízar on 31/10/20.
+//
+
+import SwiftUI
+
+@main
+struct LinkedinSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
